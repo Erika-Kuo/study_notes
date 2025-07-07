3f76e6db6a62156de51b052f5b7a5614a231e1b0
Nafridh Nafeel <nafridhnafeel@Nafridhs-MacBook-Air.local>
2025-07-07 22:16:12 +1000

    umm made notes viewable?

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import NoteDeleteView
 
 app_name = 'notes'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create_note/<int:subject_id>/', views.create_note, name='create_note'),
     path('subject/<int:subject_id>/', views.subject_detail, name='subject_detail'),
+    path('note/<int:pk>/', views.note_detail, name='note_detail'),
+    path('note/<int:pk>/edit/', views.edit_note, name='edit_note'),
+    path('note/<int:pk>/delete/', NoteDeleteView.as_view(), name='delete_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Subject, Note
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
+from .forms import NoteForm
 
 def home(request):
     subjects = Subject.objects.filter(parent__isnull=True).prefetch_related('subsubjects', 'notes')
@@ -25,3 +28,26 @@ def create_note(request, subject_id=None):
 def subject_detail(request, subject_id):
     subject = get_object_or_404(Subject.objects.prefetch_related('subsubjects', 'notes'), id=subject_id)
     return render(request, 'notes/subject_detail.html', {'subject': subject})
+
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/note_detail.html', {
+    'note': note,
+    'subject': note.subject 
+})
+
+def edit_note(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('notes:note_detail', pk=note.pk)
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/edit_note.html', {'form': form, 'note': note})
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    template_name = 'notes/note_confirm_delete.html'
+    success_url = reverse_lazy('notes:home')
